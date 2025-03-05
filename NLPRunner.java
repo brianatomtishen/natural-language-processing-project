34f1df46786d0b50f0588bc06318881b0fb8450d
brianatomtishen <btomtish0002@mymail.lausd.net>
2025-03-05 08:58:06 -0800

    Update NLPRunner.java

--- NLPRunner.java
+++ NLPRunner.java
@@ -1,9 +1,14 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class NLPRunner {
+/**
+ *  instantiates checker to run through "passwords.txt"
+ *  then starts printing them in console log 
+ *  once done it promts the user to print their own password to be checked
+ */
+    public class NLPRunner {
     public static void main(String[] args) {
-        PasswordStrengthChecker checker = new PasswordStrengthChecker("passwords.txt");
+    PasswordStrengthChecker checker = new PasswordStrengthChecker("passwords.txt");
         
         System.out.println("Analyzing passwords from file...");
         checker.analyzePasswords();
